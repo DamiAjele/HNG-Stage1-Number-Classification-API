53d3d53a521e99433875e21ca08f00d7c62fd75e
Damilola Ajele <damilolaajele05@gmail.com>
2025-02-04 12:18:58 -0800

    initial commit

--- src/index.mjs
+++ src/index.mjs
@@ -26,7 +26,8 @@ app.get("/api/classify-number", async (req, res) => {
         is_perfect: classification.isPerfect,
         properties: classification.properties,
         digit_sum: classification.digitSum,
-        fun_fact: funFact
+        fun_fact: funFact,
+        supposedFunFact: "371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371"
     });
 });
 
