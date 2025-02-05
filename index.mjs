a72856e90c05d8a8cb45539340bedde217843d53
Damilola Ajele <damilolaajele05@gmail.com>
2025-02-05 15:20:21 -0800

    updated commit

--- index.mjs
+++ index.mjs
@@ -72,7 +72,7 @@ app.get("/api/classify-number", async (req, res) => {
         return res.status(400).json({ number, error: true });
     }
 
-    if (!num) return res.status(400).json({number: "No query. Add a query Parameter", error: true})
+    if (!num) return res.status(400).json({error: true})
 
     const classification = classifyNumber(num);
     const funFact = await fetchFunFact(num);
