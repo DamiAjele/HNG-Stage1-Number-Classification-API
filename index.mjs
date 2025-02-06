bd390515d0a6f2122dc49a25a5e40e1a3625c4cf
Damilola Ajele <damilolaajele05@gmail.com>
2025-02-05 23:46:45 -0800

    updated commit

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import cors from 'cors';
 import axios from "axios";
 
 const app = express();
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 3000;
 
 app.use(cors());
 app.use(express.json());
